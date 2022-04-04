3b4fa798ac461fdc98fde4249661d805be11e8d9
EdwinGG <ecastruita@compulogic.com.mx>
2022-04-04 16:06:26 -0500

    firts commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+import { Error } from "./Error";
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+export const Formulario = ({ setMonedas }) => {
+  const [error, setError] = useState(false);
+  const [criptos, setCriptos] = useState([]);
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.Name,
+        };
+        return objeto;
+      });
+      setCriptos(arrayCriptos);
+    };
+    consultarApi();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+
+    setMonedas({ moneda, criptomoneda });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+
+      <SelectMonedas />
+      <SelectCriptomoneda />
+      {moneda}
+      <InputSubmit type="submit" value="Cotizar" />
+    </form>
+  );
+};
